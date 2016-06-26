755174d843be2b90be7c663e605d325fac3e3441
tousifcooldude <tousifcooldude@yahoo.co.in>
2016-06-26 16:33:44 +0530

    yoyoyo

--- 3rd Question/search_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `markers` (`name`, `address`, `lat`, `lng`) VALUES ('Savitribai Phule Pune University','Pune University Rd, Pune University, Ganeshkhind, Pune, Maharashtra 411007','18.5528595','73.8238257');
-INSERT INTO `markers` (`name`, `address`, `lat`, `lng`) VALUES ('Pancard Clubs','8/1, Baner Hills, Baner, Pune, Maharashtra 411045','18.5567072','73.7888434');
-INSERT INTO `markers` (`name`, `address`, `lat`, `lng`) VALUES ('D-Mart','Sr. No.106/1, Baner Road, Baner, Pune, Maharashtra 411045','18.565594','73.7674886');
-INSERT INTO `markers` (`name`, `address`, `lat`, `lng`) VALUES ('Balewadi Stadium','Balewadi stadium, Pune, Maharashtra 411045','18.572426','73.7616621');
-INSERT INTO `markers` (`name`, `address`, `lat`, `lng`) VALUES ('Smokin Joes','Shop No D/11, Sai Heritage, Vasundhara Lawns Lane, Near Medipoint Hospital, Aundh, Pune, Maharashtra 411007','18.5644993','73.8021418');
-INSERT INTO `markers` (`name`, `address`, `lat`, `lng`) VALUES ('Commissionerate of Animal Husbandry','Opposite Spicer College, Aundh Rd, Aundh, Pune, Maharashtra 411007','18.5647019','73.807916');
