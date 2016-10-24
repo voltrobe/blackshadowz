d1d56a8b6edaa43c51fa024d5237022b45033222
Santosh D <vebmate@gmail.com>
2016-10-24 23:43:05 +0530

    dbconfig

--- pages/include/dbconfig.php
+++ pages/include/dbconfig.php
@@ -2,12 +2,12 @@
 error_reporting(E_ALL ^ E_DEPRECATED);
 	 session_start();//session_start()
 date_default_timezone_set('Asia/Kolkata');
-$link=mysql_connect('localhost','root','');
-//$link=mysql_connect('localhost','blackksh_shadowu','tavor007');
+//$link=mysql_connect('localhost','root','');
+$link=mysql_connect('localhost','blackksh_shadowu','tavor007');
 if(!$link)
 {die("failed connection");}
-//$db=mysql_select_db('blackksh_shadow');
-$db=mysql_select_db('shadow');
+$db=mysql_select_db('blackksh_shadow');
+//$db=mysql_select_db('shadow');
 if(!$db)
 {die("unable to select database");}
 ?>
